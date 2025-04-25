69176ddd2335d9d8535c1b340c6b03f36a1bc21f
Edgardo <edrayoca@gmail.com>
2025-04-25 16:59:10 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vitePluginReactMetadata from "vite-plugin-react-metadata/plugin";
-
+import * as indexMetadata from "./src/meta/index";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     vitePluginReactMetadata({
       routeToFileMap: {
-        "/": "./src/meta/index",
-        "/home": "./src/meta/index",
+        "/": indexMetadata,
+        "/home": indexMetadata,
       },
     }),
   ],
