1f2c33192cc4cb81b822ecec24776e7d718cf04f
Edgardo <edrayoca@gmail.com>
2025-04-27 15:57:26 -0500

    fix

--- src/meta/product-detail.ts
+++ src/meta/product-detail.ts
@@ -1,12 +1,13 @@
 export async function generateMetadata(url?: string) {
   const defaultMetadata = {
-    title: `Vite + React + Love <3 `,
-    description: url ? url :   "TestMetadata",
+    title: `Product Detail`,
+    description: url ? url :   "Product Detail",
   };
   try {
     const response = await fetch("https://rickandmortyapi.com/api/character");
     const data = await response.json();
-    defaultMetadata.title = data.results[0]?.name || "characters";
+    defaultMetadata.title = data.results[1]?.name || "characters";
+    defaultMetadata.description = data.results[1]?.name || "characters";
   } catch (error) {
     console.error(error);
   }
